aa646636ac57088dde4728423f6758291b575e00
naoto86 <memento.mori.1986@gmail.com>
2021-04-18 23:10:21 +0900

    classの演習

--- typescript/typescript-declare-instantiate-classes/dog-class.ts
+++ typescript/typescript-declare-instantiate-classes/dog-class.ts
@@ -3,30 +3,26 @@ interface Dog {
   name: string;
   age: number;
   description: string;
-  async loadDog(id:number):Dog{
-    return await (await fetch('demoUrl')).json() as Dog;
-  }
 }
 
+class DogRecord implements Dog {
+  id: number;
+  name: string;
+  age: number;
+  description: string;
 
-class DogRecord implements Dog{
-    id:number
-    name:string
-    age:number
-    description:string
+  constructor({ name, age, description, id = 0 }: Dog) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.description = description;
+  }
 
-    constructor({name, age, description, id = 0}: Dog) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.description = description;
-    }
+  static load(id: number): DogRecord {
+    return dog;
+  }
 
-    static load(id:number):DogRecord{
-        return dog
-    }
-
-    save() {
-        // code to save dog to database
-    }
+  save() {
+    // code to save dog to database
+  }
 }
